051d042d901c4c107b9f27e94bf4adbcca2ef56e
otmanfakri <otmanhero00@gmail.com>
2024-03-29 21:44:07 -0400

    setup

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+
+from configs.BaseModel import init
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+
+# Initialise Data Model Attributes
+init()

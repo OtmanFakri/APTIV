ba6fa8b6b8ff4a0abb3c5880c191e7d468942369
root <root@fakri.com>
2024-07-04 09:26:43 -0400

    ADD AUTHORIZATION FOR ANY REQUEST

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import {en_US, provideNzI18n} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideFirebaseApp} from "@angular/fire/app";
 import {initializeApp} from "firebase/app";
@@ -15,11 +15,15 @@ import {provideMessaging} from "@angular/fire/messaging";
 import {getMessaging} from "firebase/messaging";
 import {getFirestore, provideFirestore} from "@angular/fire/firestore";
 import {getAuth, provideAuth} from "@angular/fire/auth";
+import {TokenInterceptor} from "./auth/TokenInterceptor";
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+
+    provideRouter(routes),
     provideNzI18n(en_US),
     importProvidersFrom(FormsModule),
     importProvidersFrom(
